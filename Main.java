c2490bb222fd535b3e54b575cb83a3f3351b4b43
Jaden Arceneaux <arceneauxjaden@gmail.com>
2020-02-21 04:10:07 +0000

    on

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main {
   public static void main(String[] args) {
     Sort mySort = new Sort("nums.txt");
     System.out.println("Old: " + mySort.getUnSorted());
-    ArrayList<Double> sorted = mySort.selectionSort();
+    ArrayList<Double> sorted = mySort.mergeSort();
     System.out.println("New: " + sorted.toString());
   }
 }
--- MergeSort.java
+++ MergeSort.java
@@ -1,17 +1,54 @@
 import java.util.*;
 class MergeSort{
   public ArrayList<Double> sort(ArrayList<Double> userArr){
-    ArrayList<Double> a = new ArrayList<Double>();
-    ArrayList<Double> b = new ArrayList<Double>();
-    
-    for (double item: userArr){
-      a.add(item);
+    if(userArr.size() == 1){
+      return userArr;
     }
 
-    while (a.size() > 1){
-      b.add(a.get(a.size() - 1));
-      a.remove(a.size() - 1);
+    int middle = userArr.size() / 2;
+
+    ArrayList<Double> leftArr = new ArrayList<Double>();
+    ArrayList<Double> rightArr = new ArrayList<Double>();
+
+    for(int i = 0; i < middle; i ++){
+      leftArr.add(userArr.get(i));
     }
-    return a;
+
+    for(int i = middle; i < userArr.size(); i ++){
+      rightArr.add(userArr.get(i));
+    }
+
+    
+
+    ArrayList<Double> left = sort(leftArr);
+    ArrayList<Double> right = sort(rightArr);
+    ArrayList<Double> result = merge(left, right);
+    return result;
+  }
+
+  public ArrayList<Double> merge(ArrayList<Double> left, ArrayList<Double> right){
+    ArrayList<Double> result = new ArrayList<Double>();
+
+    int lP = rP = reP = 0;
+    
+    while(lP < left.size() || rP < right.size()){
+      if(lP < left.size() && rP < right.size()){
+        if (left.get(lP) < right.get(rP)){
+          result.set(reP++, left.get(lP));
+
+        }
+        else{
+          result.set(reP++, right.get(rP++));
+        }
+      }
+      else if(lP < left.size()){
+        result.set(reP++, left.get(lP++));
+      }
+      else if(rP < right.size()){
+        result.set(reP, right.get(rP));
+      }
+    }
+
+    return result;
   }
 }
